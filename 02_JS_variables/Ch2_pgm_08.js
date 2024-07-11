1f276b865e668568575f638e296575db667e9bd8
niranjena <70028232+niranjena@users.noreply.github.com>
2024-07-11 14:41:53 +0530

    completed

---
+++ 02_JS_variables/Ch2_pgm_08.js
@@ -0,0 +1,31 @@
+// Using a variable’s current value to set its new value
+
+var score;
+score = 100;
+console.log("Your score was " + score);
+
+console.log("Great splat!!!");
+score = score + 50;
+console.log("New score: " + score);
+
+console.log("Way to go!");
+console.log("Tell the player of a successful splat");
+score1 = score + 100;
+console.log("New score:"+ score1);
+console.log("congratulations");
+
+
+/* Further Adventures
+ *
+ * The player splats a kumquat.
+ * Add code to:
+ *
+ * 1) Tell the player of a successful splat
+ *
+ * 2) Add 100 to the player's score
+ *
+ * 3) Show the new score
+ *
+ * 4) Display a message of congratulations
+ *
+ */
